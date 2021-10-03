e3e1de7a8ce9ae5d42ced38287371fcb7bbb40aa
Asit-website <asitmandal492@gmail.com>
2021-10-03 19:56:53 +0800

    node js project first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require("hbs");
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8000;
+
+// public static path
+
+const staticPath = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials"); 
+
+app.set('view engine', 'hbs');
+app.set('views',template_path);
+app.use(express.static(staticPath));
+hbs.registerPartials(partialsPath);
+
+// routing static 
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
+app.get("/weather", (req, res) => {
+    res.render("weather");
+})
+
+app.get("*", (req, res) => {
+    res.render("404",{
+        errorMsg:'Opps! page not found'
+    });
+})
+
+app.listen(port, () => {
+    console.log(`listening to the port at ${port}`)
+})
